5b12bdbb311eb3cccd5258cf7ccc2558df76398b
iCarambaaa <dkpress1916@zoho.eu>
2021-10-29 18:47:48 +0200

    validation of id uniqueness

--- src/lib/validators.js
+++ src/lib/validators.js
@@ -2,31 +2,11 @@ import { body } from "express-validator"
 import {getMedia} from '../lib/fs-tools.js'; // fs helper functions
 
 
-async function checkId(id) {
-    const content = await getMedia() // fetch Media array
-    const exists = content.find(element => element.imdbID === id)
-    if (exists) {
-        return true
-    } else {
-        return false
-    }
-}
-
-
-// TODO check uniqueness of IMDB IDs
-
 export const mediaValidationMiddlewares = [
   body("Title").exists().withMessage("Title is a mandatory field!"),
   body("Year").exists().withMessage("Year is a mandatory field!"),
-//   body("imdbID").exists().withMessage("imdbID is a mandatory and unique field!").custom((value) => {
-//       return Media.checkId(value).then(id =>{
-//           if(false) {
-//               next()
-//           } else throw new Error("ImdbID already present")
-//       })
-//   }).withMessage("imdbID has to be unique!")
+  body("imdbID").exists().withMessage("ImdbID is a mandatory field!")
 ]
-
 
 
 export const reviewsValidationMiddlewares = [
@@ -34,3 +14,17 @@ export const reviewsValidationMiddlewares = [
   body("rate").exists().withMessage("Rate is a mandatory field!").matches(/^[1-5]$/).withMessage("Rate must be 1 - 5"),
   body("elementId").exists().withMessage("Element ID is a mandatory field!") // = IMDBID
 ]
+
+
+export async function myCustomMiddlewareForCheckingUniqueness(req, res, next) {
+    const content = await getMedia()
+    
+    const exists = content.find(element => element.imdbID === req.body.imdbID)
+    
+    if (exists) {
+        res.status(400).send( "ID already exists")
+        
+    } else {
+        next()
+    }
+}
